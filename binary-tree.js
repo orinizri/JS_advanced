218e6d5bbb7a6ecb768d896b844ed964a51c30e9
orini <ori.nizri.tal@gmail.com>
2021-11-22 17:05:27 +0200

    tree

--- binary-tree.js
+++ binary-tree.js
@@ -6,13 +6,13 @@ class Node {
     }
 }
 let root_node = new Node(5); 
-// Left of root
+// Left root
 root_node.left = new Node(3);
 root_node.left.left = new Node(2);
 root_node.left.right = new Node(4);
-// Right of root
+// Right root
 root_node.right = new Node(7);
 root_node.right.left = new Node(6);
 root_node.right.right = new Node(8);
 // Observing the tree
-console.log(root_node.left.right.left);
+console.log(root_node);
